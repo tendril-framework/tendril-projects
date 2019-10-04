2d595f8010622d19438fe434e7fac6ccb5439714
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-10-04 10:41:39 +0530

    Fix doc titles

--- src/tendril/entities/projects/configsections.py
+++ src/tendril/entities/projects/configsections.py
@@ -19,8 +19,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Primitives for Project Configuration Sections
----------------------------------------------
+Project Configuration Section Elements
+--------------------------------------
 """
 
 
