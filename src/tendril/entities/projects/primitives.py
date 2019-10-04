eac71be08858416786b7518b4b0a52d4b2d97753
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-10-04 10:50:24 +0530

    Trivial doc tweak

--- src/tendril/entities/projects/primitives.py
+++ src/tendril/entities/projects/primitives.py
@@ -19,7 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Primitives related to Project Configuration elements
+Primitives related to Project Configuration Elements
 ----------------------------------------------------
 """
 
